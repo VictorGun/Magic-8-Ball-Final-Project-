669c2129e2a5c37b83983377a2a87bbeb2410b32
Victor <victor>
2021-11-18 10:23:16 -0600

    Initial Commit

---
+++ Magic 8 Ball (Final Project)/Magic_8_Ball__Final_Project_App.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_8_Ball__Final_Project_App.swift
+//  Magic 8 Ball (Final Project)
+//
+//  Created by Student on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Magic_8_Ball__Final_Project_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
